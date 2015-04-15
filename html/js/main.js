389c5fd8c561f9eb54615a28b937832a720799cd
Cesar de la Vega <cesarvegaro@gmail.com>
2015-04-15 21:47:29 +0200

    Creates structure.js and submodule TREE. Adds Tree.js to submodule TREE

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,13 @@
 (function () {
-	"use strict";
-	// Get global canvas and store it in GAMBIT
-	// GAMBIT is initialized by the library
-	GAMBIT.canvas = SVG('canvas').size(300, 300);
+    "use strict";
+    // Get global canvas and store it in GAMBIT
+    // GAMBIT is initialized by the library
+    GAMBIT.canvas = SVG('canvas').size(300, 300);
 
-	// Create a node and draw it
-	var node = new GAMBIT.Node(false, null);
-	node.draw();
+    // Create a tree and draw it
+    var tree = new GAMBIT.TREE.Tree(null);
+    // Create a node and draw it
+    var node = new GAMBIT.TREE.Node(false, null);
+    node.draw();
+    
 }());
---
+++ html/js/structure.js
@@ -0,0 +1,10 @@
+// This file loads all the modules in order to mantain a consistent structure
+// and avoid undefined errors
+
+var GAMBIT = (function () {
+	"use strict";
+	var GAMBIT = {};
+	GAMBIT.TREE = {};
+
+    return GAMBIT;
+}());
