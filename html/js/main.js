4fb07917955c401371c1f291ff0a8f471d6f8725
Cesar de la Vega <cesarvegaro@gmail.com>
2015-04-17 13:53:47 +0200

    Recursively gets number of leaves, updates positions and draws them

--- html/js/main.js
+++ html/js/main.js
@@ -5,12 +5,19 @@
     GAMBIT.canvas = SVG('canvas').size(300, 300);
 
     // Create a tree and draw it
-    var root = new GAMBIT.TREE.Node(null);
+    var root = new GAMBIT.TREE.Node(null,"padre");
     var tree = new GAMBIT.TREE.Tree(root);
     
-    var node = new GAMBIT.TREE.Node(root);
-    var node1 = new GAMBIT.TREE.Node(root);
+    var node = new GAMBIT.TREE.Node(root, "node");
+    var node1 = new GAMBIT.TREE.Node(root, "node1");
+    var node2 = new GAMBIT.TREE.Node(node1, "node2");
+    var node3 = new GAMBIT.TREE.Node(node1, "node3");
+    var node4 = new GAMBIT.TREE.Node(node3, "node4");
+    var node5 = new GAMBIT.TREE.Node(node3, "node5");
 
+    console.log(tree.numberLeaves());
+
+    tree.updatePositions();
     // Create a node and draw it
     tree.draw();
     
