4f34fa2e5f1643427550682b51f9faf57878d1a9
Cesar de la Vega <cesarvegaro@gmail.com>
2015-04-17 13:53:47 +0200

    Recursively gets number of leaves, updates positions and draws them

--- html/js/tree/Node.js
+++ html/js/tree/Node.js
@@ -1,34 +1,38 @@
 GAMBIT.TREE = (function (parentModule) {
-	"use strict";
+    "use strict";
 
-	// Node constructor
-	function Node(father) {
-		this.father = father;
-		this.terminal = true;
-		this.children = [];
-		this.x = 0;
-		
-		if (father === null) { // If this is root set level to 0
-			this.level = 0;
-		} else {
-			father.add_child(this);
-			this.level = father.level + 1;
-		}
-	}
+    // Node constructor
+    function Node(father, value) {
+        this.value = value;
+        this.father = father;
+        this.children = [];
+        this.x = 0;
+        
+        if (father === null) { // If this is root set level to 0
+            this.level = 0;
+        } else {
+            father.addChild(this);
+            this.level = father.level + 1;
+        }
+    }
 
-	// Function that draws the node in the global canvas
-	Node.prototype.draw = function () {
-		GAMBIT.canvas.circle(25).y(this.level * 50);
-	};
+    // Function that draws the node in the global canvas
+    Node.prototype.draw = function () {
+        GAMBIT.canvas.circle(25).y(this.level * 50).x(this.x);
+        console.log("Drawing " + this.value + " at y " + this.level*50 + " and x " + this.x);
+    };
 
-	// Function that adds child to node
-	Node.prototype.add_child = function (node) {
-		this.children.push(node);
-		// As there is a child now change to not terminal
-		if (this.terminal) {
-			this.terminal = false;
-		}
-	};
+    // Function that adds child to node
+    Node.prototype.addChild = function (node) {
+        this.children.push(node);
+    };
+
+    Node.prototype.isLeaf = function () {
+        if (this.children.length === 0) {
+            return true;
+        }
+        return false;
+    };
     
     // Add class to parent module
     parentModule.Node = Node;
--- html/js/tree/Tree.js
+++ html/js/tree/Tree.js
@@ -4,6 +4,7 @@ GAMBIT.TREE = (function (parentModule) {
 	// Tree constructor
 	function Tree(root) {
 		this.root = root;
+        this.positionsUpdated = false;
 	}
 
 	// Function that draws the Game in the global canvas starting from param node
@@ -11,14 +12,63 @@ GAMBIT.TREE = (function (parentModule) {
         // In case there is no arguments start from root
         if (node === undefined) { node = this.root; }
         
-		if (node.children != []) {
+		if (!node.isLeaf()) {
             for (var i = 0; i < node.children.length; i++) {
                 this.draw(node.children[i]);
             }
-            node.draw();
         }
+        node.draw();
 	};
     
+    // @return (int) the number of leaves (nodes without children) of the tree */
+    Tree.prototype.numberLeaves = function () {
+        this.leaves = [];
+        return this.recursiveNumberLeaves(this.root);
+    };
+
+    
+    // Recursive function that returns the number of leaves below a determinate node
+    // Stopping criteria: that the current node is a leaf
+    // Recursive expansion: to all of the node's children
+    Tree.prototype.recursiveNumberLeaves = function (node) {
+        if (node.isLeaf()) {
+            this.leaves.push(node);
+            return 1;
+        } else {
+            var leafCounter = 0;
+            for (var i = 0; i < node.children.length; i++) {
+                leafCounter += this.recursiveNumberLeaves(node.children[i]);
+            }
+            return leafCounter;
+        }
+    };
+
+    Tree.prototype.updatePositions = function () {
+        this.root.x = 300/2;
+        
+        this.updateLeavesPositions();
+        this.recursiveUpdatePositions(this.root);
+        this.positionsUpdated = true;
+    };
+
+    Tree.prototype.recursiveUpdatePositions = function (node) {
+        if (!node.isLeaf()) {
+            for (var i = 0; i < node.children.length; i++) {
+                this.recursiveUpdatePositions(node.children[i]);
+            }
+            // Get middle point between the children most in the left and most in the right
+            // TODO: apply level weighted function for special cases
+            node.x = node.children[0].x + (node.children[node.children.length-1].x-node.children[0].x)/2;
+        }
+    };
+    
+    Tree.prototype.updateLeavesPositions = function (){
+        var widthPerNode = 300/this.leaves.length;
+        for (var i = 0; i < this.leaves.length; i++) {
+            this.leaves[i].x = (widthPerNode*i)+(widthPerNode/2);
+        }
+    };
+
     // Add class to parent module
     parentModule.Tree = Tree;
 
