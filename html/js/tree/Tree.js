b6c7c5c3288da2a073a40f6bed1b08bdd641f3a8
Cesar de la Vega <cesarvegaro@gmail.com>
2015-05-12 18:17:04 +0100

    Adds some documentation to Tree

--- html/js/tree/Tree.js
+++ html/js/tree/Tree.js
@@ -1,13 +1,21 @@
 GAMBIT.TREE = (function (parentModule) {
 	"use strict";
 
-	// Tree constructor
+	/**
+	* Creates a new Tree.
+	* @class
+	* @param {Node} root Root node for the tree
+	*/
 	function Tree(root) {
 		this.root = root;
         this.positionsUpdated = false;
 	}
 
-    Tree.prototype.draw = function(argument){
+	/**
+	* Function that draws the Game in the global canvas starting from a node
+	* Takes care of updating the positions, clearing the canvas and drawing in it
+	*/
+    Tree.prototype.draw = function(){
         if (!this.positionsUpdated) {
             this.updatePositions();
         }
@@ -15,7 +23,13 @@ GAMBIT.TREE = (function (parentModule) {
         this.recursiveDraw();
     };
 
-	// Function that draws the Game in the global canvas starting from param node
+	/**
+	* Recursive function that draws the Game in the global canvas starting from a node
+	* If no param is given it will start from root
+	* Stopping criteria: that the current node is a leaf
+	* Recursive expansion: to all of the node's children
+	* @param {Node} [node] Node to start drawing from
+	*/
 	Tree.prototype.recursiveDraw = function (node) {
         // In case there is no arguments start from root
         if (node === undefined) { node = this.root; }
@@ -29,21 +43,34 @@ GAMBIT.TREE = (function (parentModule) {
         node.draw();
 	};
 
+	/**
+	* Function that draws a line between two given nodes
+	* @param {Node} node1 Node A
+	* @param {Node}	node2 Node B
+	*/
     Tree.prototype.drawLineBetweenNodes = function(node1, node2){
         var circleRadius = GAMBIT.CONSTANTS.CIRCLE_SIZE/2;
         GAMBIT.canvas.line(node1.x + circleRadius, node1.y + circleRadius, node2.x + circleRadius, node2.y +circleRadius).stroke({ width: 1 });
     };
 
-    // @return (int) the number of leaves (nodes without children) of the tree */
+	/**
+	* Function that returns the number of leaves in the tree
+	* @return {Number} leafCounter Number of Leaves
+	*/
     Tree.prototype.numberLeaves = function () {
         this.leaves = [];
         return this.recursiveNumberLeaves(this.root);
     };
 
 
-    // Recursive function that returns the number of leaves below a determinate node
-    // Stopping criteria: that the current node is a leaf
-    // Recursive expansion: to all of the node's children
+	/**
+	* Recursive function that returns the number of leaves below a determinate node
+	* Leaves positions are updated in a different function.
+	* Stopping criteria: that the current node is a leaf
+	* Recursive expansion: to all of the node's children
+	* @param	{Node}		node 		Node to start from
+	* @return	{Number}	leafCounter Number of Leaves
+	*/
     Tree.prototype.recursiveNumberLeaves = function (node) {
         if (node.isLeaf()) {
             this.leaves.push(node);
@@ -57,26 +84,39 @@ GAMBIT.TREE = (function (parentModule) {
         }
     };
 
+	/**
+	* Function that updates the positions of the nodes in the tree
+	*/
     Tree.prototype.updatePositions = function () {
-        // this.root.x = 250/2-GAMBIT.CONSTANTS.CIRCLE_SIZE/2;
-
         this.updateLeavesPositions();
         this.recursiveUpdatePositions(this.root);
         this.positionsUpdated = true;
     };
 
+	/**
+	* Recursive function that updates the positions of the node children.
+	* Leaves positions are updated in a different function.
+	* Stopping criteria: that the current node is a leaf
+	* Recursive expansion: to all of the node's children
+	* @param {Node} node Node that will get their children's positions updated
+	*/
     Tree.prototype.recursiveUpdatePositions = function (node) {
         if (!node.isLeaf()) {
             for (var i = 0; i < node.children.length; i++) {
                 this.recursiveUpdatePositions(node.children[i]);
             }
-            // Get middle point between the children most in the left and most in the right
+            // Get middle point between the children most in the left and most
+			// in the right
             // TODO: apply level weighted function for special cases
-            node.x = node.children[0].x + (node.children[node.children.length-1].x-node.children[0].x)/2;
+            node.x = node.children[0].x +
+				(node.children[node.children.length-1].x - node.children[0].x)/2;
             node.y = node.level * 50;
         }
     };
 
+	/**
+	* Updates the positions (x and y) of the Tree leaves
+	*/
     Tree.prototype.updateLeavesPositions = function () {
         var numberLeaves = this.numberLeaves();
         var widthPerNode = GAMBIT.canvas.viewbox().width/numberLeaves;
@@ -85,12 +125,18 @@ GAMBIT.TREE = (function (parentModule) {
             this.updateLeavesPositions();
         } else {
             for (var i = 0; i < numberLeaves; i++) {
-                this.leaves[i].x = (widthPerNode*i)+(widthPerNode/2)-GAMBIT.CONSTANTS.CIRCLE_SIZE/2;
+                this.leaves[i].x = (widthPerNode*i)+(widthPerNode/2) -
+										GAMBIT.CONSTANTS.CIRCLE_SIZE/2;
 				this.leaves[i].y = this.leaves[i].level * 50;
             }
         }
     };
 
+	/**
+	* Adds a child to a given node
+	* @param  {Node} parentNode Node that will get a new child
+	* @return {Node} newNode 	Node that has been added
+	*/
     Tree.prototype.addChildNodeTo = function (parentNode) {
         var newNode = new GAMBIT.TREE.Node(parentNode);
         if ((newNode.y + GAMBIT.CONSTANTS.CIRCLE_SIZE) > GAMBIT.canvas.viewbox().height) {
@@ -121,7 +167,9 @@ GAMBIT.TREE = (function (parentModule) {
 	};
 
 	/**
-	* Recursdive function that decreases the level of everything before a node.
+	* Recursive function that decreases the level of everything before a node.
+	* Stopping criteria: that the current node is a leaf
+	* Recursive expansion: to all of the node's children
 	* @param {Node} node Starting node
 	*/
 	Tree.prototype.recursiveDecreaseLevel = function (node) {
@@ -133,6 +181,9 @@ GAMBIT.TREE = (function (parentModule) {
 		node.level = node.level - 1;
 	};
 
+	/**
+	* Zooms out the canvas by making the viewbox bigger
+	*/
     Tree.prototype.zoomOut = function(){
         GAMBIT.canvas.viewbox(0, 0, GAMBIT.canvas.viewbox().width*1.5, GAMBIT.canvas.viewbox().height*1.5);
     };
