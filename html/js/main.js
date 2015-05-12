6ce0d33812e1bf235035f59d942654c02f336bef
Cesar de la Vega <cesarvegaro@gmail.com>
2015-05-12 17:04:58 +0100

    Adds padding top to canvas

--- html/js/main.js
+++ html/js/main.js
@@ -10,7 +10,7 @@
     // GAMBIT.tree = new GAMBIT.TREE.Tree(root);
     // // var node = GAMBIT.tree.addChildNodeTo(root);
     // // var node1 = GAMBIT.tree.addChildNodeTo(root);
-    
+
     // // var node2 = GAMBIT.tree.addChildNodeTo(node1);
     // // var node3 = GAMBIT.tree.addChildNodeTo(node1);
     // // var node4 = GAMBIT.tree.addChildNodeTo(node3);
@@ -19,5 +19,5 @@
     // GAMBIT.tree.updatePositions();
     // // Create a node and draw it
     // GAMBIT.tree.draw();
-    
+
 }());
