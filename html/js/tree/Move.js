4646484c17714a6776d85b91a09e0c6cb2b164fc
Cesar de la Vega Rodriguez <cesarvegaro@gmail.com>
2015-07-24 00:54:30 +0200

    Fixes #18. Improves automatic Move name generation

--- html/js/tree/Move.js
+++ html/js/tree/Move.js
@@ -19,12 +19,46 @@ GTE.TREE = (function (parentModule) {
         return "Move: " + "name: " + this.name;
     };
 
-    Move.prototype.compare = function (a,b) {
-        if (a.name < b.name)
-            return -1;
-        if (a.name > b.name)
-            return 1;
+    /**
+    * Public static function that compares Moves. It is designed to be called as
+    * a param for a sort function
+    * @param {Move} a Move A that will be compared
+    * @param {Move} b Move B that will be compared
+    */
+    Move.compare = function (a,b) {
+        if (a.name.length !== b.name.length) {
+            if (a.name.length < b.name.length)
+                return -1;
+            if (a.name.length > b.name.length)
+                return 1;
+        } else {
+            if (a.name < b.name)
+                return -1;
+            if (a.name > b.name)
+                return 1;
+        }
         return 0;
+    };
+
+    /**
+    * Public static function that increments a given name
+    * @param {String} str A Move name that will be incremented
+    */
+    Move.incrementName = function (str) {
+        for (var i = str.length-1; i >= 0 ; i--) {
+            var nextChar = String.fromCharCode(str.charCodeAt(i) + 1);
+            if (nextChar === "[") {
+                nextChar = "A";
+                str = str.substr(0, i) + nextChar + str.substr(i+1, str.length);
+                if (i === 0) {
+                    str = "A" + str;
+                }
+            } else {
+                str = str.substr(0, i) + nextChar + str.substr(i+1, str.length);
+                break;
+            }
+        }
+        return str;
     };
 
     Move.prototype.draw = function(parent, child) {
