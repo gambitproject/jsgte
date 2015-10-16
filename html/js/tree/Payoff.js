74327e36c932ef53ecb133c591efb7e83603a360
Cesar de la Vega Rodriguez <cesarvegaro@gmail.com>
2015-10-16 17:27:02 +0200

    Adds payoff documentation

--- html/js/tree/Payoff.js
+++ html/js/tree/Payoff.js
@@ -4,7 +4,8 @@ GTE.TREE = (function(parentModule) {
     /**
      * Creates a new Payoff.
      * @class
-     * @param {Node} leaf Leaf where Payoff is attached
+     * @param {Node}   leaf   Leaf where Payoff is attached
+     * @param {Player} player Player that gets this payoff
      */
     function Payoff(leaf, player) {
         this.value = null;
@@ -46,30 +47,39 @@ GTE.TREE = (function(parentModule) {
 
     /**
      * Changes the text of the editable label
+     * @param {String} text New payoff's text
      */
     Payoff.prototype.changeText = function(text) {
-        var value = parseFloat(text, 10); // Remove leading 0
+        // Remove leading 0
+        var value = parseFloat(text, 10);
+        // Check if it's a fraction
         var split = text.split('/');
         if (split.length === 2) {
             value = parseInt(split[0], 10) / parseInt(split[1], 10);
         }
+        // Check if it is not a number
         if (!isNaN(value)) {
+            // Set the value
             this.setValue(value);
+            // If decimal
             if (value % 1 !== 0) {
+                // Display the label as a fraction
                 this.text = new GTE.TREE.UTILS.Fraction(value, false).toString();
             } else {
+                // If it is not decimal, display the value itself
                 this.text = this.value;
             }
             if (this.editable !== null) {
+                // Update the ContentEditable text
                 this.editable.setText(this.text);
             }
         } else {
             window.alert("Payoff should be a number.");
+            // In order to get the previous text back, change text to current this.text
             this.changeText(this.text);
         }
         return this.value;
     };
-
 
     // Add class to parent module
     parentModule.Payoff = Payoff;
