c97943d82b44cf3b015c89920fd510fd140afa3d
Harkirat Singh <harkirat96@gmail.com>
2016-05-23 23:48:47 +0530

    Added functions to strategic and strategicBlock

--- html/js/strategic/StrategyBlock.js
+++ html/js/strategic/StrategyBlock.js
@@ -5,9 +5,28 @@ GTE.TREE = (function(parentModule) {
      * Creates a new Strategy.
      * @class
      */
-    function StrategyBlock() {
-        
+    function StrategyBlock(x, y, players, moves) {
+        this.strategy = new GTE.TREE.Strategy(players, moves);
+        this.x = x;
+        this.y = y;
+        this.shape = null;
     }
+
+    StrategyBlock.prototype.assignPayoffs = function() {
+        this.strategy.findPayoff(GTE.tree.root);
+    };
+
+    StrategyBlock.prototype.draw = function() {
+        var x = (100) * (this.x + 1);
+        var y = (100) * (this.y + 1);
+        this.shape = GTE.canvas.rect(100, 100)
+            .fill({
+                color: '#9d9d9d'
+            });
+        this.shape.translate(x, y);
+        this.strategy.payoffs[0].draw(x, y+40);
+        this.strategy.payoffs[1].draw(x+40, y);
+    };
 
     // Add class to parent module
     parentModule.StrategyBlock = StrategyBlock;
