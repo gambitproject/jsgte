389f216efaa01b321b2b219abe317fcb5d7ed678
Harkirat Singh <harkirat96@gmail.com>
2016-05-23 14:30:20 +0530

    Added strategy.md

---
+++ html/Matrix.js
@@ -0,0 +1,16 @@
+GTE.TREE = (function(parentModule) {
+    "use strict";
+
+    /**
+     * Creates a new Strategy.
+     * @class
+     */
+    function Matrix() {
+        
+    }
+
+    // Add class to parent module
+    parentModule.Matrix = Matrix;
+
+    return parentModule;
+}(GTE.TREE)); // Add to GTE.TREE sub-module
