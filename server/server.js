cee0f9e019c16329d35450a9e88f8fb7ca12ca5b
Harkirat Singh <harkirat96@gmail.com>
2016-06-06 02:10:45 +0530

    Modified server to handle post requests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }))
+
 var http = require('http').Server(app);
 app.set('port',(process.env.PORT||3000));
 app.use(express.static('../html/'));
@@ -29,5 +35,9 @@ app.get('/strategic', function (req, res)
 	res.sendFile('/html/strategic.html',{'root' : '../'});
 });
 
-
-
+app.post('/solve', function (req, res) 
+{
+	//solve and return ans
+	res.send("ans");
+	console.log(req.body['Name']);
+});
