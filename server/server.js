900bb5d053f40e4e694757c92fbf7935bc6aadcf
Harkirat Singh <harkirat96@gmail.com>
2016-06-06 19:04:07 +0530

    Shifted from spawn to exec

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
-app.use(bodyParser.urlencoded({ extended: true }))
-var spawn = require('child_process').spawn;
+app.use(bodyParser.urlencoded({ extended: true }));
+var exec = require('child_process').exec;
 var http = require('http').Server(app);
 app.set('port',(process.env.PORT||3000));
 app.use(express.static('../html/'));
@@ -43,22 +43,21 @@ app.post('/solve', function (req, res)
 	var input = matrix['height'] + " "+matrix['width']+"\n";
 	input = input + matrix['player1'] + "\n";
 	input = input + matrix['player2'];
-	console.log(input);
 	var index = gameIndex;
 	gameIndex++;
 	fs.writeFile("./inputs/game"+index, input, function(err) {
 	    if(err) {
 	        return console.log(err);
 	    }
-	    console.log("The input was saved!");
-		var process = spawn('./lrslib-061/lrsnash',  ['./inputs/game'+index]);
-		process.stdout.on('data', function(data) {
-			res.send(data);
-			fs.writeFile("./outputs/game"+index, data, function(err) {
+	    console.log("The input was saved to ./inputs/game"+index);
+	    exec('./lrslib-061/lrsnash ./inputs/game'+index, function(err, stdout, stderr) {
+		    if (err) throw err;
+			res.send(stdout);
+		    fs.writeFile("./outputs/game"+index, stdout, function(err) {
 			    if(err) {
 			        return console.log(err);
 			    }
-			    console.log("The output was saved!");
+			    console.log("The output was saved to ./outputs/game"+index);
 			});
 		});
 	});
