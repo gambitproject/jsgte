317e943924745cdd5457c4c98e5159d7174a1617
Harkirat Singh <harkirat96@gmail.com>
2016-06-06 17:48:47 +0530

    Added functions to store input recieved and calculated output

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
@@ -12,7 +13,7 @@ app.use(express.static('../html/'));
 app.set('layout');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
+var gameIndex = 0;
 http.listen (app.get('port'),function(){
   console.log("listening to port number "+app.get('port'));
   console.log("tree format at -> localhost:"+app.get('port')+"/tree");
@@ -38,6 +39,26 @@ app.get('/strategic', function (req, res)
 app.post('/solve', function (req, res) 
 {
 	//solve and return ans
+	var input = "3 2 0 6 2 5 3 3 1 0 0 2 4 3 ";
+	var index = gameIndex;
+	gameIndex++;
+	fs.writeFile("./inputs/game"+index, input, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log("The input was saved!");
+		var process = spawn('./lrslib-061/lrsnash',  ['./inputs/game'+index]);
+		process.stdout.on('data', function(data) {
+			console.log(data);
+			fs.writeFile("./outputs/game"+index, data, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    console.log("The output was saved!");
+			});
+
+		});
+	});
 	res.send("ans");
 	console.log(req.body);
 });
