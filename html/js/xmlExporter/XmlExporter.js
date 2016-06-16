a466ea166f2a6cfb2649d21c90f0c0d32231c8e1
Harkirat Singh <harkirat96@gmail.com>
2016-06-16 19:29:50 +0530

    Added XmlExporter class

---
+++ html/js/xmlExporter/XmlExporter.js
@@ -0,0 +1,15 @@
+GTE.TREE = (function (parentModule) {
+    "use strict";
+
+    /**
+    * Creates a new xml Exporter
+    * @class
+    */
+    function XmlExporter() {
+
+    }
+
+    // Add class to parent module
+    parentModule.XmlExporter = XmlExporter;
+    return parentModule;
+}(GTE.TREE)); // Add to GTE.TREE sub-module
