6b3879db32a7239c469be6871f4f676de76561ae
Harkirat Singh <harkirat96@gmail.com>
2016-06-16 20:18:47 +0530

    Added buttons and basic exporting prototypes to XmlExporter class

--- html/js/xmlExporter/XmlExporter.js
+++ html/js/xmlExporter/XmlExporter.js
@@ -6,8 +6,53 @@ GTE.TREE = (function (parentModule) {
     * @class
     */
     function XmlExporter() {
+        this.tree = "";
+    };
 
-    }
+    XmlExporter.prototype.exportTree = function() {
+        this.exportGTE();
+    };
+
+    XmlExporter.prototype.exportGTE = function() {
+        this.startProperty("gte", {version : "0.1"}, 0);
+        this.endProperty("gte", 0);
+    };
+
+    XmlExporter.prototype.startProperty = function(property_name, parameters, tab) {
+        var append = this.assignTab(tab);
+        append += "<"+property_name+" ";
+        for(var parameter in parameters) {
+            append += parameter;
+            append += "=\"";
+            append += parameters[parameter];
+            append += "\"";
+            append += " ";
+        }
+        append += ">\n";
+        this.tree += append;
+    };
+
+    XmlExporter.prototype.endProperty = function(property_name, tab) {
+        var append = this.assignTab(tab);
+        append += "<"+property_name+">\n";
+        this.tree += append;
+    };
+
+    XmlExporter.prototype.assignTab = function(tab) {
+        var string = "";
+        for(var i = 0; i<tab; i++) {
+            string = string + "\t";
+        }
+        return string;
+    };
+
+    XmlExporter.prototype.exportDisplayProperties = function(tab_space) {
+
+    };
+
+    XmlExporter.prototype.toString = function() {
+        console.log(this.tree);
+    };
 
     // Add class to parent module
     parentModule.XmlExporter = XmlExporter;
