9970368fa56cf930a867ef8de573397c244745d2
Harkirat Singh <harkirat96@gmail.com>
2016-06-17 06:29:35 +0530

    Added display properties

--- html/js/xmlExporter/XmlExporter.js
+++ html/js/xmlExporter/XmlExporter.js
@@ -15,18 +15,18 @@ GTE.TREE = (function (parentModule) {
 
     XmlExporter.prototype.exportGTE = function() {
         this.startProperty("gte", {version : "0.1"}, 0);
+        this.exportDisplayProperties();
         this.endProperty("gte", 0);
     };
 
-    XmlExporter.prototype.startProperty = function(property_name, parameters, tab) {
+    XmlExporter.prototype.startProperty = function(property_name, parameters, tab, body) {
         var append = this.assignTab(tab);
-        append += "<"+property_name+" ";
+        append += "<"+property_name;
         for(var parameter in parameters) {
-            append += parameter;
+            append += (" "+parameter);
             append += "=\"";
             append += parameters[parameter];
             append += "\"";
-            append += " ";
         }
         append += ">\n";
         this.tree += append;
@@ -34,9 +34,16 @@ GTE.TREE = (function (parentModule) {
 
     XmlExporter.prototype.endProperty = function(property_name, tab) {
         var append = this.assignTab(tab);
-        append += "<"+property_name+">\n";
+        append += "</"+property_name+">\n";
         this.tree += append;
     };
+
+    XmlExporter.prototype.addBody = function(property_name, tab) {
+        var append = this.assignTab(tab);
+        append += property_name+"\n";
+        this.tree += append;
+    };
+
 
     XmlExporter.prototype.assignTab = function(tab) {
         var string = "";
@@ -47,7 +54,33 @@ GTE.TREE = (function (parentModule) {
     };
 
     XmlExporter.prototype.exportDisplayProperties = function(tab_space) {
+        this.startProperty("display", {}, 1);
+        for(var i = 1; i<GTE.tree.players.length; i++) {
+            this.startProperty("color", {player : i}, 2);
+            this.addBody(GTE.tree.players[i].colour, 3);
+            this.endProperty("color", 2);
+        }
+        this.startProperty("font", {}, 2);
+        this.addBody("Times", 3);
+        this.endProperty("font", 2);
 
+        this.startProperty("strokeWidth", {}, 2);
+        this.addBody(GTE.STORAGE.settingsLineThickness, 3);
+        this.endProperty("strokeWidth", 2);
+
+        this.startProperty("nodeDiameter", {}, 2);
+        this.addBody(GTE.STORAGE.settingsCircleSize, 3);
+        this.endProperty("nodeDiameter", 2);
+
+        this.startProperty("isetDiameter", {}, 2);
+        this.addBody(GTE.STORAGE.settingsLineThickness, 3);
+        this.endProperty("isetDiameter", 2);
+
+        this.startProperty("levelDistance", {}, 2);
+        this.addBody(GTE.STORAGE.settingsDistLevels, 3);
+        this.endProperty("levelDistance", 2);
+
+        this.endProperty("display",1);
     };
 
     XmlExporter.prototype.toString = function() {
