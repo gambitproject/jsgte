9a4066e0193577b3fbeebcb13e3e0bd60884547f
amelie <amelie.heliou@polytechnique.edu>
2016-08-10 22:31:08 +0200

    Finding points in the envelope, TODO drawing everything

--- html/js/eqdiagrams/3D.js
+++ html/js/eqdiagrams/3D.js
@@ -1,8 +1,22 @@
-function projection(vector) {
+var eps=0.0001; //error to zero;
+var color=[]; //strategy's color
+
+//TODO
+function draw_canvas(i){ //draw the canvas of the 3D drawing for player i
+    
+}
+
+//TODO
+function draw_plan([p1,p2,p3],i,j){ //draw the payoff plan for player i strategy y
+    
+}
+
+function projection(vector,i) { //from theory to reality
+    var shift=Number(2*this.margin+this.width);
     var vec0=[0,0];
     var vec1=[0,1];
     var vec2=[1/2,0.866];
-    return [vector[2]*vec2[0],Number(vector[1]*vec1[1]+vector[2]*vec2[1])];
+    return [Number(vector[2]*vec2[0]+i*shift),Number(vector[1]*vec1[1]+vector[2]*vec2[1])];
 }
 
 function add(vec1, vec2){
@@ -28,14 +42,14 @@ function scal(vec1,vec2){
 
 function equal (vec1, vec2){
     for (var i=0;i<3;i++){
-        if ((vec1[i]-vec2[i])*(vec1[i]-vec2[i])>0.00000001)
+        if ((vec1[i]-vec2[i])*(vec1[i]-vec2[i])>eps*eps)
             return false;
     }
     return true;
 }
 
 function equal_num (vec1, vec2){
-    if ((vec1-vec2)*(vec1-vec2)>0.00000001)
+    if ((vec1-vec2)*(vec1-vec2)>eps*eps)
         return false;
     return true;
 }
@@ -50,12 +64,12 @@ function normalize (vec){
     return mul(1/norm,vec);
 }
 
-function plan_intersect ([p1,p2,p3],[q1,q2,q3]){
+function plan_intersect ([p1,p2,p3],[q1,q2,q3]){ //compute the intersection between two plans
     var p_nor=cross(sub(p1,p2),sub(p1,p3));
     var q_nor=cross(sub(q1,q2),sub(q1,q3));
     if (is_parallel(p_nor,q_nor) && !equal(p1,q1)) //there is no intersection
         return null;
-    if (is_parallel(p_nor,q_nor))
+    if (is_parallel(p_nor,q_nor)) //plans are equals
         return "all"
     var dir= cross (p_nor,q_nor);
     dir=normalize(dir);
@@ -120,16 +134,30 @@ function line_plan_intersect ([u,p1],[q1,q2,q3]){ //p1 is a point on the line, u
     return add(p1,mul(coeff,u));
 }
 
-function is_possible (vec){
-    var eps=0.0001
+function z_coor([x,y], [p1,p2,p3]){ //return the z-coordinate of the point (x,y) in the plan [p1,p2,p3]
+    //console.log(p1+","+p2+","+p3);
+    var p_nor=cross(sub(p1,p2),sub(p1,p3));
+    if (equal_num(p_nor[2],0))
+        return -1; //there is no point with x,y coordinate in this plan
+    return Number(scal(p1,p_nor)-x*p_nor[0]-y*p_nor[1])/Number(p_nor[2]);
+}
+
+function is_possible (vec,plan){ //check if vec is a point in the convex envelope.
     if( vec[0]>Number(1+eps) || vec[0]<Number(0-eps) ||vec[1]>Number(1+eps) || vec[1]<Number(0-eps))
         return false;
     if (vec[0]+vec[1]>Number(1+eps))
         return false;
+    for (var i=0;i<plan.length;i++){
+        var temp=z_coor([vec[0],vec[1]],plan[i]);
+        if (vec[2]==2)
+        console.log(vec[2]+" "+temp+" "+plan[i]);
+        if (vec[2]<Number(temp-eps))
+            return false;
+    }
     return true;
 }
 
-function compute_best_reponse(player){
+function compute_best_reponse(player){ //main function uses all previous functions
     var nb_strat=GTE.diag.nb_strat[player];
     var payoffs=[];
     var plan=[[[0,0,0],[0,1,0],[1,0,0]],[[0,0,0],[0,1,0],[0,1,1]],[[0,0,0],[1,0,1],[1,0,0]],[[0,1,1],[0,1,0],[1,0,0]]];
@@ -175,7 +203,7 @@ function compute_best_reponse(player){
             }
         }
     }
-    console.log(lines);
+    //console.log(lines);
     //computing intersection of all pairs of lines and plan.
     var points=[];
     var points_to_plan=[];
@@ -215,7 +243,7 @@ function compute_best_reponse(player){
             }
         }
     }
-    console.log(points);
+    //console.log(points);
     //check for unicity and inside points
     var u_points=[];
     var u_points_to_plan=[];
@@ -223,7 +251,7 @@ function compute_best_reponse(player){
     for (var i=0;i<plan.length;i++)
         u_plan_to_points.push([]);
     for (var i=0;i<points.length-1;i++){
-        if (is_possible(points[i])){
+        if (is_possible(points[i],plan)){
             var test=true;
             for (var j=i+1;j<points.length;j++){
                 if (equal(points[i],points[j])){
@@ -246,25 +274,28 @@ function compute_best_reponse(player){
                 u_points_to_plan.push([]);
                 for (var k=0;k<points_to_plan[i].length;k++){
                     u_points_to_plan[u_points_to_plan.length-1].push(points_to_plan[i][k]);
-                    u_plan_to_points[points_to_plan[i][k]].push(u_points_to_plan.length-1);
+                    u_plan_to_points[points_to_plan[i][k]].push(u_points_to_plan[u_points_to_plan.length-1]);
                 }
             }
         }
     }
     
     var i=points.length-1;
-    if (is_possible(points[i])){
+    if (is_possible(points[i], plan)){
         u_points.push(points[i]);
         u_points_to_plan.push([]);
         for (var k=0;k<points_to_plan[i].length;k++){
             u_points_to_plan[u_points_to_plan.length-1].push(points_to_plan[i][k]);
-            u_plan_to_points[points_to_plan[i][k]].push(u_points_to_plan.length-1);
+            u_plan_to_points[points_to_plan[i][k]].push(u_points_to_plan[u_points_to_plan.length-1]);
         }
     }
-    console.log(u_points);
-    
-    
-    
-    
+    //console.log(u_points);
+    for (var i=0;i<u_points_to_plan.length; i++){
+        draw_envelope(u_points_to_plan[i]);
+    }
+}
+
+//TODO
+function draw_envelope(u_points_to_plan[i]){ //draw the faces of the upper envelope. Based on the graham algorithm
     
 }
